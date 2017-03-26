3e7253c7336db66ad79c41887b4d9452587522e5
HGonzalez <hugisma@gmail.com>
2017-03-26 10:25:37 -0400

    se agrega la clase Articulo.java

---
+++ src/main/java/org/owl/hsystem/modelos/Articulo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.owl.hsystem.modelos;
+
+/**
+ *
+ * @author huguito
+ */
+public class Articulo {
+    private String codigo;
+    private String descripcion;
+    private double precio;
+    private double porcentajeImpuesto;
+    private Long id;
+    private int existencia;
+}
