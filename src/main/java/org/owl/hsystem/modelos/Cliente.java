7799e46f6177dfa38fb1ff307e380e70a1dc5ef1
hgonzalez <hgonzalez@cabal.com.py>
2017-04-04 13:46:55 -0400

    Creacion de las clases persona y cliente

---
+++ src/main/java/org/owl/hsystem/modelos/Cliente.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.owl.hsystem.modelos;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Desarr_Hugo
+ */
+public class Cliente extends Persona{
+    private Date fechaAlta;
+    private Date fechaBaja;
+    private char estado;
+    
+    
+    public Cliente(){
+        
+    }
+
+    public Date getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public void setFechaAlta(Date fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+
+    public Date getFechaBaja() {
+        return fechaBaja;
+    }
+
+    public void setFechaBaja(Date fechaBaja) {
+        this.fechaBaja = fechaBaja;
+    }
+
+    public char getEstado() {
+        return estado;
+    }
+
+    public void setEstado(char estado) {
+        this.estado = estado;
+    }
+}
---
+++ src/main/java/org/owl/hsystem/modelos/Persona.java
@@ -0,0 +1,153 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.owl.hsystem.modelos;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Desarr_Hugo
+ */
+public class Persona implements Identificable{
+    private String nombre;
+    private String apellido;
+    private String razonSocial;
+    private String ruc;
+    private String numeroDocumento;
+    private String direccion;
+    private String telefono;
+    private String email;
+    private char sexo;
+    private String edad;
+    private Long id;
+    
+    public Persona(){
+        
+    }
+    
+    private String getNombre(){
+        return nombre;
+    }
+    
+    private void SetNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    private String getApellido(){
+        return apellido;
+    }
+    
+    private void setApellido(String apellido){
+        this.apellido = apellido;
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public String getRuc() {
+        return ruc;
+    }
+
+    public void setRuc(String ruc) {
+        this.ruc = ruc;
+    }
+
+    public String getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(String numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Persona other = (Persona) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        String sexoStr = sexo == 'M' ? "Masculino" : "Femenino";
+        return id + "\t" + numeroDocumento + "\t" + nombre + "\t" + apellido + "\t" + sexoStr + "\t" + telefono + "\t" + email;
+    }
+    
+    
+}
